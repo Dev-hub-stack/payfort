7c3e146371efc9e675446d287694925a11c4aafa
Rameez <rajaunleashed@gmail.com>
2020-10-13 00:45:09 +0500

    fixed localhost url

--- contants.php
+++ contants.php
@@ -10,8 +10,8 @@ define('PAYFORT_SHA_REQUEST_PHRASE', '$2y$10$/CgKp4goZ');
 define('PAYFORT_SHA_RESPONSE_PHRASE', '$2y$10$RF7WRs3TN');
 define('CURRENCY', 'AED');
 define('SANDBOX_MODE', true);
-//define('WEB_URL', 'https://wheel.ahmadhassan.design/');
-define('WEB_URL', 'http://localhost:4200/');
+define('WEB_URL', 'https://wheel.ahmadhassan.design/');
+//define('WEB_URL', 'http://localhost:4200/');
 
 //DB Credentials
 define('HOST', 'localhost');
