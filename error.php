b2d07058f321990296a2c0da9cd36f1c22822e29
Rameez <rajaunleashed@gmail.com>
2020-10-22 23:51:04 +0500

    removed header

--- error.php
+++ error.php
@@ -1,8 +1,33 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(0);
-header('Location: ' . WEB_URL . '/cancel-order');
+//header('Location: ' . WEB_URL . '/cancel-order');
 ?>
-<script>
-    window.location.href = 'https://tunerstop.com/cancel-order';
-</script>
+
+<?php include('header.php') ?>
+
+<section class="nav">
+    <ul>
+        <li class="lead"> Payment Method</li>
+        <li class="active lead"> Done</li>
+    </ul>
+</section>
+<section class="confirmation">
+    <label class="failed" for="">Payment Failed</label>
+    <!-- <label class="failed" for="" >Failed</label> -->
+    <small>Error while processing your payment</small>
+</section>
+
+<div class="h-seperator"></div>
+
+<?php if (isset($_REQUEST['error_msg'])) { ?>
+    <section>
+        <div class="error">error message : error_msg</div>
+    </section>
+    <div class="h-seperator"></div>
+
+
+<?php } ?>
+
+<section class="actions">
+    <a class="btm" href="https://payment.ahmadhassan.design">New Order</a>
+</section>
+<?php include('footer.php') ?>
