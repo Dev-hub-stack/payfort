8619044acd74c0a69b534de1173810813f42b767
Rameez <rajaunleashed@gmail.com>
2020-10-24 13:32:31 +0500

    fixed amount

--- route.php
+++ route.php
@@ -3,13 +3,15 @@
 /**
  * @copyright Copyright PayFort 2012-2016 
  */
-
 if(!isset($_REQUEST['r'])) {
     echo 'Page Not Found!';
     exit;
 }
 require_once 'PayfortIntegration.php';
 $objFort = new PayfortIntegration();
+session_start();
+$cart = getCart($_SESSION['session_id'], $_SESSION['order_number']);
+$objFort->amount = $cart['total'] + $cart['shipping'] - $cart['discount'];
 if($_REQUEST['r'] == 'getPaymentPage') {
     $objFort->processRequest(htmlspecialchars($_REQUEST['paymentMethod'], ENT_QUOTES, 'UTF-8'));
 }
