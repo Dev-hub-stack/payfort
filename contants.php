ae0f323611d1b8f2aa9b3dcf887f74565ff2ded0
root <root@localhost.localdomain>
2021-01-25 18:29:08 +0000

    staging

--- contants.php
+++ contants.php
@@ -10,8 +10,8 @@ define('PAYFORT_SHA_REQUEST_PHRASE', '$2y$10$/CgKp4goZ');
 define('PAYFORT_SHA_RESPONSE_PHRASE', '$2y$10$RF7WRs3TN');
 define('CURRENCY', 'AED');
 define('SANDBOX_MODE', true);
-define('WEB_URL', 'https://wheel.ahmadhassan.design/');
-define('API_URL', 'https://ahmadhassan.design/api');
+define('WEB_URL', 'https://wheel.ahmadhassan.tech/');
+define('API_URL', 'https://ahmadhassan.tech/api');
 //define('API_URL', 'http://localhost:8000/api');
 //define('WEB_URL', 'http://localhost:4200/');
 
