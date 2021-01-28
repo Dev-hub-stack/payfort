cf76d0b5eb431a4349819afcb4e1e8aebbdc5c83
Ahmad Hassan <s2016-112@bnu.edu.pk>
2021-01-28 13:07:42 +0500

    merge master to production

--- contants.php
+++ contants.php
@@ -1,43 +1,29 @@
 <?php
 
 //define('BASE_PATH', '/payfort-payment');
-
-define('BASE_PATH', '/');
+define('BASE_PATH', '');
 define('GATEWAY_HOST', 'https://checkout.payfort.com/');
 define('GATEWAY_SANDBOX_HOST', 'https://sbcheckout.payfort.com/');
-define('MERCHANT_IDENTIFIER', 'ceef86d7');
-define('ACCESS_CODE', '4mqZKs0sVVClvw9CEMws');
-define('PAYFORT_SHA_REQUEST_PHRASE', '$2y$10$/CgKp4goZ');
-define('PAYFORT_SHA_RESPONSE_PHRASE', '$2y$10$RF7WRs3TN');
-
-
-/* Production Config.... - Start */
-
-// define('BASE_PATH', '');
-// define('GATEWAY_HOST', 'https://checkout.payfort.com/');
-// define('GATEWAY_SANDBOX_HOST', 'https://sbcheckout.payfort.com/');
-// define('MERCHANT_IDENTIFIER', 'SZwMYSVm');
-// define('ACCESS_CODE', 'YrTEkNCWmYAkbxXtCCNf');
-// define('PAYFORT_SHA_REQUEST_PHRASE', '$2y$10$zNrzxksc/');
-// define('PAYFORT_SHA_RESPONSE_PHRASE', '$2y$10$dr0uio8Ha');
-
-/* Production Config.... - End */
-
+//define('MERCHANT_IDENTIFIER', 'ceef86d7');
+define('MERCHANT_IDENTIFIER', 'SZwMYSVm');
+//define('ACCESS_CODE', '4mqZKs0sVVClvw9CEMws');
+define('ACCESS_CODE', 'YrTEkNCWmYAkbxXtCCNf');
+//define('PAYFORT_SHA_REQUEST_PHRASE', '$2y$10$/CgKp4goZ');
+define('PAYFORT_SHA_REQUEST_PHRASE', '$2y$10$zNrzxksc/');
+//define('PAYFORT_SHA_RESPONSE_PHRASE', '$2y$10$RF7WRs3TN');
+define('PAYFORT_SHA_RESPONSE_PHRASE', '$2y$10$dr0uio8Ha');
 define('CURRENCY', 'AED');
-
-define('SANDBOX_MODE', true);
-define('WEB_URL', 'https://wheel.ahmadhassan.tech/');
-define('API_URL', 'https://ahmadhassan.tech/api');
-
+define('SANDBOX_MODE', false);
+define('WEB_URL', 'https://tunerstop.com/');
+define('API_URL', 'https://admin.tunerstop.com/api');
 //define('API_URL', 'http://localhost:8000/api');
 //define('WEB_URL', 'http://localhost:4200/');
 
 //DB Credentials
-//define('HOST', 'localhost');
 define('HOST', 'localhost');
-define('USER', 'root');
-define('DB', 'tuner');
-define('PASSWORD', 'kePFut#3!0Lux$');
+define('USER', 'nexuswebstudio');
+define('DB', 'tunerstop2020');
+define('PASSWORD', 'S?h4eXK[H]Mf');
 //define('USER', 'root');
 //define('DB', 'tuner_stop');
 //define('PASSWORD', '4slash1234');
